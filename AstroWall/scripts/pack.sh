8fcd5b53a40b3d35cdf0950f57630cd873031e05
BWiegell <bj.wiegell@gmail.com>
2022-11-03 13:29:56 +0100

    reorganize package build stuff

--- AstroWall/scripts/pack.sh
+++ AstroWall/scripts/pack.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pkgbuild --root ./bin/Release/Astro\ Wall.app --scripts ./bin/PackageScripts --identifier com.astro.wall.Astro-Wall --install-location /Applications/Astro\ Wall.app ./bin/Package/com.astro.wall.Astro-Wall.pkg
-productbuild --distribution bin/Package/distribution.dist --product obj/Release/Product.plist  --package-path ./bin/Package/ --resources ./bin/Resources/  ./bin/Package/Astro.pkg
+pkgbuild --root ./bin/Release/Astro\ Wall.app --scripts ./pkgsrc/scripts --identifier com.astro.wall.Astro-Wall --install-location /Applications/Astro\ Wall.app ./bin/Package/com.astro.wall.Astro-Wall.pkg
+productbuild --distribution ./pkgsrc/distribution.dist --product pkgsrc/Product.plist --package-path ./bin/Package/ --resources ./pkgsrc/resources/  ./bin/Package/Astro.pkg
